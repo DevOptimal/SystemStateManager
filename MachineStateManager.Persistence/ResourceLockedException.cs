1324dd32cb88674da910cb67603e6848d6b979e0
Brad White <4261702+bradselw@users.noreply.github.com>
2022-06-23 09:30:01 -0700

    Throw custom exception when resource is locked (#7)

---
+++ MachineStateManager.Persistence/ResourceLockedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bradselw.MachineStateManager.Persistence
+{
+    public class ResourceLockedException : Exception
+    {
+        public ResourceLockedException()
+        {
+        }
+
+        public ResourceLockedException(string message)
+            : base(message)
+        {
+        }
+
+        public ResourceLockedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
